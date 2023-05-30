bc2a174df8fd05140c414c5ef70ceb9008e74048
Akshay3875 <121480219+Akshay3875@users.noreply.github.com>
2023-05-30 13:18:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Article from './Article';
+import './App.css'; // Import the CSS file for App component
+
+const App = () => {
+  return (
+    <div className="App">
+      <Article />
+    </div>
+  );
+};
+
+export default App;
